c6b80d485141f135706e7f0b6cf9ed0832d1fe14
Daniel Hartman <dphart9@gmail.com>
2020-09-12 23:44:07 -0400

    hmm

--- src/iosTest/kotlin/Test.kt
+++ src/iosTest/kotlin/Test.kt
@@ -1,3 +1,13 @@
-class Test {
+import kotlinx.coroutines.runBlocking
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
+class Test {
+    @Test
+    fun run(){
+        runBlocking {
+            val hello = HelloWorld()
+            assertEquals(hello.getCarouselAds("166"),"fasdfdsfdfasdfsdf sad" )
+        }
+    }
 }
