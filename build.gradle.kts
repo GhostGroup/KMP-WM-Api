daca0ff12d37eb97782695b796a46869c8e8dccc
Daniel Hartman <dphart9@gmail.com>
2020-09-12 23:39:07 -0400

    wip

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,16 +25,14 @@ kotlin {
             }
         }
     }
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    iosX64("ios") {
+        binaries {
+            framework {
+                baseName = "library"
+            }
+        }
     }
 
-    
     sourceSets {
         val commonMain by getting {
             dependencies {
@@ -68,7 +66,10 @@ kotlin {
                 implementation(kotlin("test-js"))
             }
         }
-        val nativeMain by getting
-        val nativeTest by getting
+        val iosMain by getting {
+            dependencies {
+            }
+        }
+        val iosTest by getting
     }
 }
--- build/js/packages/WmApi-test/adapter-browser.js
+++
@@ -1,2 +0,0 @@
-require("C:\\Users\\Daniel\\IdeaProjects\\WmApi\\build\\js\\node_modules\\kotlin-test-js-runner\\kotlin-test-karma-runner.js")
-module.exports = require("C:\\Users\\Daniel\\IdeaProjects\\WmApi\\build\\js\\packages\\WmApi-test\\kotlin\\WmApi-test.js")
