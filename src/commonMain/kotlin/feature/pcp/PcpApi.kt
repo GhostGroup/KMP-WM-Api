83dd0a391eccdc79a10e1a7da1dcf356cd764ca8
Daniel Hartman <dphart9@gmail.com>
2020-12-07 18:03:48 -0500

    tags now

--- src/commonMain/kotlin/feature/pcp/PcpApi.kt
+++ src/commonMain/kotlin/feature/pcp/PcpApi.kt
@@ -2,8 +2,10 @@ package feature.pcp
 
 import WmResult
 import core.listings.ListingApi
+import core.listings.ListingResponse
 import feature.product.ProductsResponse
-import feature.product.getTrendingProductsRoute
+import feature.product.getTrendingProductsForCategoryRoute
+import feature.product.getTrendingProductsForTagRoute
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import kotlinx.coroutines.CompletableDeferred
@@ -13,25 +15,38 @@ import toWmResult
 class PcpApi {
 
     suspend fun getPcpCategory(location: Location): PcpModel {
-        val categories:WmResult<ProductCategoriesResponse> = InternalWmApiDependencies.httpClient
+        val categories: WmResult<ProductCategoriesResponse> = InternalWmApiDependencies.httpClient
             .get<HttpResponse>(getCategoriesApiUrl(location.toLatLngQueryFormat()))
             .toWmResult(InternalWmApiDependencies.json)
-        val arrayOfCategories:Array<Category> = categories.getDataOrNull()?.data?.categories ?: arrayOf()
+        val arrayOfCategories: Array<Category> = categories.getDataOrNull()?.data?.categories ?: arrayOf()
         val pcpModel = PcpModel(arrayOfCategories.map { PcpCategory(it, null) }.toTypedArray())
         return pcpModel
     }
 
-    suspend fun getPcpSubcategory(categoryUuid: String, location:Location): Array<PcpSubCategory> {
-        val trendingProductsMap:HashMap<String,Deferred<WmResult<ProductsResponse>>> = HashMap()
+    suspend fun getPcpSubcategory(categoryUuid: String, location: Location): Array<PcpSubCategory> {
+        val trendingProductsMap: HashMap<String, Deferred<WmResult<ProductsResponse>>> = HashMap()
+        val nearbyListingsMap:HashMap<String, Deferred<WmResult<ListingResponse>>>
 
-        val subcategories:WmResult<ProductCategoriesResponse> = InternalWmApiDependencies.httpClient
+        val subcategories: WmResult<ProductCategoriesResponse> = InternalWmApiDependencies.httpClient
             .get<HttpResponse>(getSubcategoriesRoute(categoryUuid, location))
             .toWmResult(InternalWmApiDependencies.json)
-        val subcategoriesFirstFive =  subcategories.getDataOrNull()?.data?.categories?.filter { it.uuid != null }?.take(5)
+        val subcategoriesFirstFive =
+            subcategories.getDataOrNull()?.data?.categories?.filter { it.uuid != null }?.take(4)
         subcategoriesFirstFive?.let {
-            for (cat in it){
-                trendingProductsMap[cat.uuid!!] = CompletableDeferred(InternalWmApiDependencies.httpClient
-                    .get<HttpResponse>(getTrendingProductsRoute(location, cat.uuid, 30)).toWmResult(InternalWmApiDependencies.json))
+            for (cat in it) {
+                if (cat.facet_type == "tag") {
+                    trendingProductsMap[cat.uuid!!] = CompletableDeferred(
+                        InternalWmApiDependencies.httpClient
+                            .get<HttpResponse>(getTrendingProductsForTagRoute(location, cat.uuid, 50))
+                            .toWmResult(InternalWmApiDependencies.json)
+                    )
+                } else {
+                    trendingProductsMap[cat.uuid!!] = CompletableDeferred(
+                        InternalWmApiDependencies.httpClient
+                            .get<HttpResponse>(getTrendingProductsForCategoryRoute(location, cat.uuid, 50))
+                            .toWmResult(InternalWmApiDependencies.json)
+                    )
+                }
             }
         }
 
@@ -41,14 +56,15 @@ class PcpApi {
             ?.map {
                 val trending = trendingProductsMap[it.uuid!!]?.await()?.getDataOrNull()?.data?.products
                 PcpSubCategory(
-                category = it,
-                learnExcept = null,
-                learnImage = null,
-                topRatedProduct = trending?.firstOrNull(),
-                popularProducts = trending?.filterIndexed { index, product -> index > 0 }?.toTypedArray(),
-                nearbyListings = nearbyListings.await().getDataOrNull()?.data?.listings
-            ) }
+                    category = it,
+                    learnExcept = null,
+                    learnImage = null,
+                    topRatedProduct = trending?.firstOrNull(),
+                    popularProducts = trending?.filterIndexed { index, product -> index > 0 }?.toTypedArray(),
+                    nearbyListings = nearbyListings.await().getDataOrNull()?.data?.listings
+                )
+            }
             ?.toTypedArray()
-                ?: arrayOf()
+            ?: arrayOf()
     }
 }
--- src/commonMain/kotlin/feature/product/ProductRoutes.kt
+++ src/commonMain/kotlin/feature/product/ProductRoutes.kt
@@ -2,6 +2,26 @@ package feature.product
 
 import feature.pcp.Location
 
-fun getTrendingProductsRoute(location:Location, categoryUuid:String, boundingRadius:Int = 50)=
-    "${WmApiDependencies.baseUrl}/discovery/v1/products/trending?&latlng=${location.toLatLngQueryFormat()}" +
-            "&filter[any_category_uuids][]=$categoryUuid&filter[bounding_radius]=${boundingRadius}mi"
+fun getTrendingProductsForTagRoute(location:Location, tagUUID:String?,  boundingRadius:Int = 50):String {
+
+    val tagFilter = if (tagUUID != null)"filter[any_tag_uuids][]=$tagUUID" else null
+
+    val baseUrl = "${WmApiDependencies.baseUrl}/discovery/v1/products/trending?&latlng=${location.toLatLngQueryFormat()}&filter[bounding_radius]=${boundingRadius}mi"
+
+    return  addQueryParam(baseUrl, tagFilter)
+}
+
+fun getTrendingProductsForCategoryRoute(location:Location, categoryUuid:String?,  boundingRadius:Int = 50):String {
+
+    val categoryFilter = if (categoryUuid != null) "filter[any_category_uuids][]=$categoryUuid" else null
+
+    val baseUrl = "${WmApiDependencies.baseUrl}/discovery/v1/products/trending?&latlng=${location.toLatLngQueryFormat()}&filter[bounding_radius]=${boundingRadius}mi"
+
+    return addQueryParam(baseUrl, categoryFilter)
+
+}
+
+fun addQueryParam(url:String, queryParam:String?):String {
+    if(queryParam == null) return url
+    return "${url}&$queryParam"
+}
