e6bf3558e92652fafc64785f59ec02a2ad84844e
Daniel Hartman <dphart9@gmail.com>
2021-01-08 17:23:44 -0500

    fix

--- src/commonMain/kotlin/feature/pcp/PcpRoutes.kt
+++ src/commonMain/kotlin/feature/pcp/PcpRoutes.kt
@@ -1,6 +1,6 @@
 package feature.pcp
 
-private val categoriesApiUrl = "${WmApiDependencies.baseUrl}/discovery/v1/categories?include[]=subcategories&filter[location]=any"
+private val categoriesApiUrl = "${WmApiDependencies.baseUrl}/discovery/v1/categories?include[]=subcategories"
 
 fun getCategoriesApiUrl(latlng:String):String = "${categoriesApiUrl}&latlng=${latlng}"
 fun getSubcategoriesRoute(uuid:String, location: Location) = "${WmApiDependencies.baseUrl}/discovery/v1/categories/$uuid/subcategories?include%5B%5D=parent&latlng=${location.toLatLngQueryFormat()}&sort_by=total_products_count"
