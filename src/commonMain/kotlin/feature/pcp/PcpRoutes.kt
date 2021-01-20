69885202385b4c621283190b9a6841cd2abc73b2
Daniel Hartman <dphart9@gmail.com>
2021-01-20 16:16:38 -0500

    updated query parms

--- src/commonMain/kotlin/feature/pcp/PcpRoutes.kt
+++ src/commonMain/kotlin/feature/pcp/PcpRoutes.kt
@@ -2,7 +2,7 @@ package feature.pcp
 
 private val categoriesApiUrl = "${WmApiDependencies.baseUrl}/discovery/v1/categories?include[]=subcategories"
 
-fun getCategoriesApiUrl(latlng:String):String = "${categoriesApiUrl}&latlng=${latlng}"
+fun getCategoriesApiUrl(latlng:String):String = "${categoriesApiUrl}&latlng=${latlng}&filter[exclude_slugs][]=pre-roll"
 fun getSubcategoriesRoute(uuid:String, location: Location) = "${WmApiDependencies.baseUrl}/discovery/v1/categories/$uuid/subcategories?include%5B%5D=parent&latlng=${location.toLatLngQueryFormat()}&sort_by=total_products_count"
 
 
